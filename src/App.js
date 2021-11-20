d8e8f25b085ba61922882a93fae6500860ab6f4c
LeeJaeHoon <dlwogns3413@naver.com>
2021-11-21 01:34:02 +0900

    create Movie component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -23,16 +24,12 @@ function App() {
       ) : (
         <div>
           {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h1>{movie.title}</h1>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre, i) => (
-                  <li key={i}>{genre}</li>
-                ))}
-              </ul>
-            </div>
+            <Movie
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} />
+      <h1>{title}</h1>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre, i) => (
+          <li key={i}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Movie;
